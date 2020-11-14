c33c8000db6a836c81ba59f4754513f16a775c0a
Seamul <islamseamul.com>
2020-11-14 17:07:02 +0600

    sudhu msg ta baki ache

--- enroll/urls.py
+++ enroll/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home),
-    path('save/',views.save_data,name="save")
+    path('save/',views.save_data,name="save"),
+    path('delete/',views.delete_data,name="delete"),
+    path('edit/',views.edit_data,name="edit")
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -14,11 +14,39 @@ def save_data(request):
     form= StudentForm()
     print("I am in form")
     # if form.is_valid():
+    sid=request.POST.get('stuid')
     name=request.POST['name']
     email=request.POST['email']
     password=request.POST['password']
     print(name,email,password)
-    user=User(name=name,email=email,password=password)
-    user.save()
-    return JsonResponse({'status':"save"})
     
+    if(sid==''):
+        user=User(name=name,email=email,password=password)
+        
+    else:
+        user=User(id=sid,name=name,email=email,password=password)
+    
+    user.save()
+
+    
+    student_data= User.objects.values()
+    student_data=list(student_data)
+    return JsonResponse({'status':"save","student_data":student_data})
+    
+def delete_data(request):
+    if request.method=="POST":
+        id=request.POST.get('sid')
+        print(id)
+        pi= User.objects.get(pk=id)
+        pi.delete()
+        return JsonResponse({"status":1})
+    return JsonResponse({"status":0})
+
+def edit_data(request):
+    if request.method=="POST":
+        id=request.POST.get('sid')
+        print(id)
+        student= User.objects.get(pk=id)
+        student_data={'id':student.id,'name':student.name,'email':student.email, 'password':student.password}
+        return JsonResponse(student_data)
+    return JsonResponse({"status":0})
